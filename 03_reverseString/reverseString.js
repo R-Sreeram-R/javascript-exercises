72db74176fbc2bac3452b988c5ec8eaa77e1ff70
R-Sreeram-R <sreeramragha@gmail.com>
2025-02-27 11:23:59 +0400

    ADDED 3-7 soln

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var letters = string.split("");
+    var newString ="";
+    for (let i = letters.length-1; i >=0 ;i--){
+        newString += letters[i];
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {  
+    
+
+      
+    return array.filter(num => !args.includes(num)); 
+  
 };
 
 // Do not edit below this line
